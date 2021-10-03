72db7d670b9b24d3c1068e4556b173430717266f
kaller <rexrayo15@gmail.com>
2021-10-03 12:22:20 -0400

    App Theme

---
+++ src/theme/appTheme.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    globalMargin: {
+        marginHorizontal: 20
+    },
+    pokeballBG: {
+        position: 'absolute',
+        top: -100,
+        right: -100,
+        opacity: 0.2,
+        width: 300,
+        height: 300
+    },
+    title: {
+        fontSize: 35,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: 'black'
+    }
+
+});
